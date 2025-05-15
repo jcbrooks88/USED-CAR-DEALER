8a5b1b2a1136e600f5bc69b884ae0571dff10a88
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-05-15 19:49:50 -0400

    postgres db update

--- server/src/config/connections.ts
+++ server/src/config/connections.ts
@@ -2,56 +2,58 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Sequelize } from 'sequelize';
-// COMMENT OUT BELOW TO RUN LOCAL HOST
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false, 
-        },
-      },
-    })
-  :new Sequelize(
-      process.env.DB_NAME as string,
-      process.env.DB_USER as string,
-      process.env.DB_PASSWORD as string,
-      {
-        host: process.env.DB_HOST || 'localhost',
-        port: parseInt(process.env.DB_PORT as string, 10) || 5432,
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-        pool: {
-          max: 10,
-          min: 0,
-          acquire: 30000,
-          idle: 10000,
-        },
-      }
-    );
-// UNCOMMENT THIS, AND COMMENT OUT ABOVE TO RUN ON LOCAL HOST
-    // const sequelize = new Sequelize(
-    //   process.env.DB_NAME as string,
-    //   process.env.DB_USER as string,
-    //   process.env.DB_PASSWORD as string,
-    //   {
-    //     host: process.env.DB_HOST || 'localhost',
-    //     port: parseInt(process.env.DB_PORT as string, 10) || 5432,
-    //     dialect: 'postgres',
-    //     dialectOptions: {
-    //       decimalNumbers: true,
-    //     },
-    //     pool: {
-    //       max: 10,
-    //       min: 0,
-    //       acquire: 30000,
-    //       idle: 10000,
-    //     },
-    //   }
-    // );
+
+//Production - COMMENT OUT BELOW TO RUN LOCAL HOST
+// const sequelize = process.env.DATABASE_URL
+//  ? new Sequelize(process.env.DATABASE_URL, {
+//      dialect: 'postgres',
+//      dialectOptions: {
+//        ssl: {
+//          require: true,
+//          rejectUnauthorized: false, 
+//        },
+//      },
+//    })
+//  :new Sequelize(
+//      process.env.DB_NAME as string,
+//      process.env.DB_USER as string,
+//      process.env.DB_PASSWORD as string,
+//      {
+//        host: process.env.DB_HOST || 'localhost',
+//        port: parseInt(process.env.DB_PORT as string, 10) || 5432,
+//        dialect: 'postgres',
+//        dialectOptions: {
+//          decimalNumbers: true,
+//        },
+//        pool: {
+//          max: 10,
+//          min: 0,
+//          acquire: 30000,
+//          idle: 10000,
+//        },
+//      }
+//    );
+
+//Development - UNCOMMENT THIS, AND COMMENT OUT ABOVE TO RUN ON LOCAL HOST
+     const sequelize = new Sequelize(
+       process.env.DB_NAME as string,
+       process.env.DB_USER as string,
+       process.env.DB_PASSWORD as string,
+       {
+         host: process.env.DB_HOST || 'localhost',
+         port: parseInt(process.env.DB_PORT as string, 10) || 5432,
+         dialect: 'postgres',
+         dialectOptions: {
+           decimalNumbers: true,
+         },
+         pool: {
+           max: 10,
+           min: 0,
+           acquire: 30000,
+           idle: 10000,
+         },
+       }
+     );
 
 
 sequelize.authenticate()
